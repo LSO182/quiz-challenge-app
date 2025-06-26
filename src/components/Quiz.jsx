a4ef0d9d98954a12db041cddb98592e7971f7fc0
sanchezolmedoluciano@gmail.com <sanchezolmedoluciano@gmail.com>
2025-06-26 12:09:07 -0300

    feat: quiz componente, shuffled the answers

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,10 +1,32 @@
 import { useState } from "react";
 
+import Questions from "../../questions.js";
 
 export default function Quiz() {
   const [userAnwsers, setUserAnswers] = useState([]);
-
   const activeQuestionIndex = userAnwsers.length;
+  const shuffledAnswers = [...Questions[activeQuestionIndex].answers];
+  shuffledAnswers.sort(() => Math.random() - 0.5);
+  function handleSelectAnswer(selectedAnswer) {
+    setUserAnswers((prevUserAnswers) => {
+      return [...prevUserAnswers, selectedAnswer];
+    });
+  }
 
-  return <p>asdasd</p>;
+  return (
+    <div id="quiz">
+      <div id="question">
+        <h2>{Questions[activeQuestionIndex].text}</h2>
+        <ul id="answers">
+          {shuffledAnswers.map((answer) => (
+            <li key={answer} className="answer">
+              <button onClick={() => handleSelectAnswer(answer)}>
+                {answer}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
