85d62c85be0f59661332cfb1d8bbfd8d40d3f3d8
sanchezolmedoluciano@gmail.com <sanchezolmedoluciano@gmail.com>
2025-06-26 11:03:11 -0300

    feat: firs basic structure componentes and question file

---
+++ src/components/Quiz.jsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+
+
+export default function Quiz() {
+  const [userAnwsers, setUserAnswers] = useState([]);
+
+  const activeQuestionIndex = userAnwsers.length;
+
+  return <p>asdasd</p>;
+}
